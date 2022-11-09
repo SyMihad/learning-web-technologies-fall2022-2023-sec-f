3c4e5ab031cd7a1f47e0559a97e5cb19fe423e02
SyMihad <symihad@gmail.com>
2022-11-09 15:51:59 +0600

    3:51

--- lab_06_database/taskB/display.php
+++ lab_06_database/taskB/display.php
@@ -1,7 +1,7 @@
 <?php
-
-
-
+    $con = mysqli_connect('localhost', 'root','', 'webtech');
+    $sql = "SELECT * from  product_db";
+    $result = mysqli_query($con, $sql);
 ?>
 
 <html>
@@ -14,11 +14,15 @@
             <tr>
                 <td><b>Name</b></td>
                 <td><b>Profit</b></td>
-                <td>
-                    
-                </td>
-
+                <td></td>
             </tr>
+            
+            <?php
+                while($row = mysqli_fetch_array($result)){
+                   print("<tr><td>$row['name']</td><td>$row['buyingPrice']</td><td>$row['sellingPrice']</td><td>/<td></tr>");
+                }
+            ?>
+            
         </table>
     </body>
 </html>
