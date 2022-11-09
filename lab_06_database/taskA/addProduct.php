6fa355a1637a3815f9641cd1e4d9b21c7f241c90
SyMihad <symihad@gmail.com>
2022-11-09 15:23:27 +0600

    3:20

---
+++ lab_06_database/taskA/addProduct.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+?>
+
+<html>
+    <head>
+        <title>Add Product</title>
+    </head>
+
+    <body>
+        <form>
+            <fieldset>
+                <legend>ADD PRODUCT</legend>
+                Name<br>
+                <input type='text' name='name' value=''><br>
+                Buying Price:<br>
+                <input type='number' name='buyingPrice' value=''><br>
+                Selling Price:<br>
+                <input type='number' name='sellingPrice' value=''><br>
+                <hr>
+                <input type='submit' name='submit' value='Save'>
+            </fieldset>
+        </form>
+    </body>
+</html>
